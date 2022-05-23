25828cbd341de19778efb33cff1596207d16d18c
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-23 13:02:17 -0400

    Feat: criacao da migration "articles"

---
+++ alpha-blog/db/migrate/20220523165745_create_articles.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# articles Migration
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
