a27fae5beb857b3f96cbdae031b56629e2018c96
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-25 17:36:17 -0400

    Feat: users table and model

--- db/migrate/20220525213027_create_users.rb
+++ db/migrate/20220525213027_create_users.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+# Migration for create_table Users
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :email
       t.timestamps
     end
   end
