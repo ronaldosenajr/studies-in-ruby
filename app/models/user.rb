b456b30666feb835be26006b8d1be8aad231503b
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-25 18:14:42 -0400

    Feat: model user com validacoes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
