6abae81e96492efb7cc9f380c0aa9bd65112cbc0
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-26 10:17:37 -0400

    Feat: email do usuario agora e salvo todo em downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# User model
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
