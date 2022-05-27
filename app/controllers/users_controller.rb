3cf52b05f0eafc7e7c2637684a1645fdaeeb3e68
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-27 11:38:22 -0400

    Feat: session do user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
