11835885f2302849612f9fb4ffbd27d5a2c49347
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-30 08:43:55 -0400

    Feat: helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 
 # class for Users
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -26,12 +26,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account was updated successfully'
       redirect_to @user
@@ -44,5 +41,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
