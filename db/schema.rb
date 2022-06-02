b7cd6ea7de2a7ec65f1d765fdfe700df149e1a1e
ronaldo junior <ronaldosenajr@gmail.com>
2022-06-02 10:17:24 -0400

    Feat: migration para categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_530_182_113) do
-  create_table 'articles', force: :cascade do |t|
-    t.string 'title'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.text 'description'
-    t.integer 'user_id'
+ActiveRecord::Schema.define(version: 2022_06_02_140633) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.integer "user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.boolean 'admin', default: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+  end
+
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 # a class named category_test that inherits from ActiveSupport::TestCase
 class CategoryTest < ActiveSupport::TestCase
   test 'category should be valid' do
-    @category = Category.new('Sports')
+    @category = Category.new(name: 'Sports')
     assert @category.valid?
   end
 end
